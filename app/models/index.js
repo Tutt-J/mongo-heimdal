13a360ebbdbf169c62e39492d11d98ce08edc284
Wy <wy@mail.com>
2024-04-17 10:12:04 +0200

    connect to db

---
+++ app/models/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI,{ ssl : false })
+.then(() => console.log('MongoDB connected !'))
+.catch(() => console.log('Erreur with MongoDB connection'));
