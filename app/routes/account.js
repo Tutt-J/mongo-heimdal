e9cc198fbd8a9f10abbb855592b700e8e3e3abd4
Wy <wy@mail.com>
2024-04-18 15:20:05 +0200

    signup, login and auth middleware

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express();
-
+const auth = require("../middlewares/auth.js");
 const accountCtrl = require("../controllers/account.js");
 
-router.get("/", accountCtrl.readAll);
-router.post("/", accountCtrl.create);
-router.put("/:id", accountCtrl.update);
-router.delete("/:id", accountCtrl.delete);
+router.get("/", auth, accountCtrl.readAll);
+router.post("/", auth, accountCtrl.create);
+router.put("/:id", auth, accountCtrl.update);
+router.delete("/:id", auth, accountCtrl.delete);
 
 module.exports = router;
