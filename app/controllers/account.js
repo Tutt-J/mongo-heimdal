6c191650e5703cf3fd8ae54f31898ec6951add68
Wy <wy@mail.com>
2024-04-18 17:25:40 +0200

    findAll, create and update account with middlewares

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -1,16 +1,62 @@
-exports.readAll = (req, res) => {
-    res.send("Welcome to account readAll")
-}
+const Account = require("../models/account.js");
 
-exports.create = (req, res) => {
-    res.send("Welcome to account create")
-}
+exports.readAll = async (req, res) => {
+  try {
+    const accounts = await Account.find({ userId: req.auth.userId });
+    res.status(200).json(accounts);
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message || "Some error occurred while retrieving accounts.",
+    });
+  }
+};
 
-exports.update = (req, res) => {
-    res.send("Welcome to account update")
-}
+exports.create = async (req, res) => {
+  try {
+    const { bankName, customName } = req.body;
+    const account = new Account({
+      bankName,
+      customName,
+      userId: req.auth.userId,
+    });
+    await account.save();
+    res.status(201).json(account);
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message || "Some error occurred while creating account.",
+    });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const { bankName, customName } = req.body;
+    const account = await Account.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        userId: req.auth.userId,
+      },
+      {
+        bankName,
+        customName,
+      },
+      {
+        returnDocument: "after",
+      }
+    );
+
+    if (!account) {
+      return res.status(404).json({ message: "Account not found." });
+    }
+
+    res.status(200).json(account);
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message || "Some error occurred while updating account.",
+    });
+  }
+};
 
 exports.delete = (req, res) => {
-    res.send("Welcome to account delete")
-}
-
+  res.send("Welcome to account delete");
+};
--- app/models/account.js
+++ app/models/account.js
@@ -1,26 +1,35 @@
 const mongoose = require("mongoose");
 
 const accountSchema = new mongoose.Schema({
-    bankName: {
-        type: String,
-        trim: true,
-        required: [true, "Bank name is required."]
-    },
-    customName: {
-        type: String,
-        trim: true,
-        maxlength: [50, "Custom name must be at most 50 characters long."],
-        required: [true, "Custom name is required."]
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: [true, "UserId is required."]
-    },
-    lastUpdated: {
-        type: Date,
-        required: [true, "Last Updated is required"]
-    }
+  bankName: {
+    type: String,
+    trim: true,
+    required: [true, "Bank name is required."],
+  },
+  customName: {
+    type: String,
+    trim: true,
+    maxlength: [50, "Custom name must be at most 50 characters long."],
+    required: [true, "Custom name is required."],
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "UserId is required."],
+  },
+  lastUpdated: {
+    type: Date,
+  },
+});
+
+accountSchema.pre("save", function (next) {
+  this.lastUpdated = Date.now();
+  next();
+});
+
+accountSchema.pre("findOneAndUpdate", function (next) {
+  this.set({ lastUpdated: Date.now() });
+  next();
 });
 
 const Account = mongoose.model("Account", accountSchema);
